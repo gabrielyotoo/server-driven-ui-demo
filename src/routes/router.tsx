0d166c0530b2938a1fc84d0b88840e99ed0ee97d
Gabriel Freitas Yamamoto <gabriel.yamamoto@mathex.com.br>
2026-04-13 23:44:41 -0300

    feat: add routing

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,33 +1,41 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { navigationRef } from './navigation';
+import { ServerDrivenScreen } from '../server-driven-ui';
 
 export enum MainStackScreenNames {
+  Home = 'Home',
   Pix = 'Pix',
   Notifications = 'Notifications',
 }
 
 export type RootStackParams = {
+  [MainStackScreenNames.Home]: undefined;
   [MainStackScreenNames.Pix]: undefined;
   [MainStackScreenNames.Notifications]: undefined;
 };
 
 const RootStack = createNativeStackNavigator<RootStackParams>();
 
+const Home = () => <ServerDrivenScreen screenName="Home" />;
+const Pix = () => <ServerDrivenScreen screenName="Pix" />;
+const Notifications = () => <ServerDrivenScreen screenName="Notifications" />;
+
 export function Router() {
   return (
     <NavigationContainer ref={navigationRef}>
       <RootStack.Navigator>
         <RootStack.Screen
-          name={MainStackScreenNames.Pix}
+          name={MainStackScreenNames.Home}
           options={{
             headerShown: false,
           }}
-          component={Search}
+          component={Home}
         />
+        <RootStack.Screen name={MainStackScreenNames.Pix} component={Pix} />
         <RootStack.Screen
           name={MainStackScreenNames.Notifications}
-          component={List}
+          component={Notifications}
         />
       </RootStack.Navigator>
     </NavigationContainer>
